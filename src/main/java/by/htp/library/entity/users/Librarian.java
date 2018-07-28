5d15cfdea7997ad6e8e10637b67d807c0e723d74
Kris <celly@inbox.ru>
2018-07-28 23:23:03 +0300

    Librarian & Reader classes are created

--- src/main/java/by/htp/library/entity/users/Librarian.java
+++ src/main/java/by/htp/library/entity/users/Librarian.java
@@ -1,7 +1,54 @@
 package by.htp.library.entity.users;
 
-public class Librarian extends AbstractUser{
-	
+public class Librarian extends AbstractUser {
+
 	private String login;
+
+	public Librarian() {
+		super();
+	}
+
+	public Librarian(String login, String password) {
+		super(password);
+		this.login = login;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((login == null) ? 0 : login.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Librarian other = (Librarian) obj;
+		if (login == null) {
+			if (other.login != null)
+				return false;
+		} else if (!login.equals(other.login))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Librarian [login=" + login + "]";
+	}
 
 }
--- src/main/java/by/htp/library/entity/users/Reader.java
+++ src/main/java/by/htp/library/entity/users/Reader.java
@@ -1,7 +1,107 @@
 package by.htp.library.entity.users;
 
-public class Reader extends AbstractUser{
-	
+public class Reader extends AbstractUser {
+
 	private int login;
+	private String firstName;
+	private String lastName;
+	private int phoneNumber;
+
+	public Reader() {
+		super();
+	}
+
+	public Reader(String password, int login, String firstName, String lastName, int phoneNumber) {
+		super(password);
+		this.login = login;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public Reader(int login, String firstName, String lastName, int phoneNumber) {
+		super();
+		this.login = login;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public int getLogin() {
+		return login;
+	}
+
+	public void setLogin(int login) {
+		this.login = login;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + login;
+		result = prime * result + phoneNumber;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Reader other = (Reader) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (login != other.login)
+			return false;
+		if (phoneNumber != other.phoneNumber)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Reader [login=" + login + ", firstName=" + firstName + ", lastName=" + lastName + ", phoneNumber="
+				+ phoneNumber + "]";
+	}
+	
+	
 
 }
